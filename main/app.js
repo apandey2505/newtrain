ccd42e5c0e21ee6f257ec876fa556be610e521c2
Abhishek Pandey <apandey@techracers.io>
2019-06-27 15:36:32 +0530

    hh

---
+++ main/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("hhhh");
+});
+
+
+app.listen(3000,()=> {
+    console.log("server running");
+});
